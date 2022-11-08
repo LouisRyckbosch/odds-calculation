312814cbf37327214b8cb79958334fb4a8932c56
Louis <louis.ryckbosch@gmail.com>
2022-11-08 18:51:43 +0100

    link quote and group nmp

--- src/arbitrage_bet/new_matching_process.clj
+++ src/arbitrage_bet/new_matching_process.clj
@@ -5,12 +5,36 @@
 (defn gen-id [quote]
   (str (:match quote) "|" (:site quote)))
 
+(defn calc-ratio [x y]
+  (if (> x y)
+    (/ x y)
+    (/ y x)))
+
+(defn collect-quote [quote]
+  {:quoteTeam1 (:quoteTeam1 quote)
+   :quoteTeam2 (:quoteTeam2 quote)
+   :quoteDraw  (:quoteDraw quote)}
+  )
+
+(defn quote-too-good? [match quote]
+  (let [match-quote (collect-quote match)
+        result (reduce
+                 (fn [x [key value]]
+                   (+ (calc-ratio (key quote) value) x))
+                 0
+                 match-quote)]
+    (< 4.5 result)))
+
 (defn link-if-best [quote quote2]
-  (let [leven-score (l/levenshtein quote quote2)
-        previous-score (:leven-score quote)]
-    (if (< previous-score leven-score)
-      (conj quote {:leven-score leven-score
-                   :best-match  (gen-id quote2)}))))
+  (if (or (= (:site quote) (:site quote2))
+          (quote-too-good? quote quote2))
+    quote
+    (let [leven-score (l/levenshtein (:match quote) (:match quote2))
+          previous-score (:leven-score quote)]
+      (if (> previous-score leven-score)
+        (conj quote {:leven-score leven-score
+                     :best-match  (gen-id quote2)})
+        quote))))
 
 (defn add-closest-match [quote quotes]
   (let [quote (conj quote {:leven-score 999
@@ -19,15 +43,18 @@
             quote
             quotes)))
 
-(defn match-quotes-between-them [quotes]
+(defn link-quotes-between-them [quotes]
   (reduce (fn [result quote]
             (conj result (add-closest-match quote quotes)))
           []
           quotes))
 
+(defn quotes-by-match [quotes]
+  )
+
 (defn trunc-time-from-date [quote]
   (let [[y m d] (java-time.core/as quote :year :month-of-year :day-of-month)]
-    (java-time.api/local-date-time y m d 0 0)))
+    (keyword (.toString (java-time.api/local-date-time y m d 0 0)))))
 
 (defn add-quote [result-map key quote]
   (conj result-map {key (conj (get result-map key) quote)}))
@@ -48,10 +75,46 @@
   (filter #(and (not (nil? (:date %)))
                 (not (= (:date %) :undefined))) quotes))
 
-(defn test [quotes]
-  (match-quotes-between-them (second quotes)))
+(defn link-quotes-by-name [quotes]
+  (reduce
+    (fn [col quote-for-one-day]
+      (conj col (link-quotes-single-day quote-for-one-day)))
+    []
+    (val quotes)))
+
+(defn create-match [quote]
+  {:date             (:date quote)
+   :name             (gen-name quote)
+   :nameTeam1        (:nameTeam1 quote)
+   :nameTeam2        (:nameTeam2 quote)
+   :quotes           [quote]})
+
+(defn add-quote [match quote]
+  (conj match {:quotes (conj (:quotes match) quote)}))
+
+(defn add-to-no-match [quote result]
+  (let [undefined (first (filter #(= (:match %) :undefined) result))]
+    (if (nil? undefined)
+      (conj (create-match quote) {:name :undefined})
+      (add-quote undefined quote))))
+
+(defn add-to-results [quote match result]
+  
+  )
+
+(defn regroup-quote [quote quotes results]
+  (let [match (first (filter #(match-with-quote quote) quotes))]
+    (if (nil? match)
+      (add-to-no-match quote results)
+      (add-to-results quote match result))))
+
+(defn regroup-quotes-by-links [quotes]
+  (reduce
+    (fn [col ]))
+  )
 
 (defn matching-process-with-info [quotes]
   (-> (filter-nil-date quotes)
       (map-quote-per-day)
-      (test)))
+      (link-quotes-by-name)
+      (regroup-quotes-by-link)))
